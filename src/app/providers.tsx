ce3e73fa4db375dca9efe6f3656eb72aa6d12c07
hwanfront <kim015jh@gmail.com>
2025-05-21 15:05:39 +0900

    feat: msw 설정 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,15 @@
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/config/queryClient'
-import { type ReactNode } from 'react'
-import { useAuthInit } from '@/features/auth/hooks/useAuthInit'
+import { Suspense, type ReactNode } from 'react'
+import { useMSW } from '@/hooks/useMSW'
 
 export default function Providers({ children }: { children: ReactNode }) {
-  useAuthInit()
+  useMSW()
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <Suspense fallback={null}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </Suspense>
   )
 }
