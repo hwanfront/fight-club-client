a0893549f82f3527a63257beadf4f88989b2fb2b
hwanfront <kim015jh@gmail.com>
2025-08-02 21:26:23 +0900

    feat: Svg sprite 파일 캐시 버스팅 추가

--- src/components/icon/SvgIcon.tsx
+++ src/components/icon/SvgIcon.tsx
@@ -7,10 +7,16 @@ type SvgIconProps = {
   className?: string
 }
 
-export function SvgIcon({ name, width, height, className }: SvgIconProps) {
+const spriteVersion = process.env.NEXT_PUBLIC_SPRITE_VERSION
+
+export function SvgIcon({ name, className }: SvgIconProps) {
+  const spriteUrl = spriteVersion
+    ? `/sprite.svg?v=${spriteVersion}#${name}`
+    : `/sprite.svg#${name}`
+
   return (
-    <svg className={className} width={width} height={height}>
-      <use href={`/sprite.svg?#${name}`} />
+    <svg className={className} viewBox="0 0 35 35">
+      <use href={spriteUrl} />
     </svg>
   )
 }
